870b6dc950a400a2af3eac883d1d88126ee083a2
Slawomir Serafin <margolek555@gmail.com>
2023-11-28 18:24:37 +0100

    modify the existing solution by adding possibility to use list as a key

--- threshold-handler/validating_nested_json.py
+++ threshold-handler/validating_nested_json.py
@@ -1,11 +1,12 @@
 
 
 def validate_data(data, allowed_keys, level=""):
+    print(f"DATA KEYS: {data.keys()}")
     for key in data.keys():
         if key not in allowed_keys:
             print(f"Error: Key '{key}' not allowed at level '{level}'")
             return False
-        if isinstance(data[key], list):
+        if isinstance(data[key], list) and any(isinstance(value, dict) for value in data[key]):
             for index, item in enumerate(data[key]):
                 if not isinstance(item, dict):
                     print(f"Error: Invalid item '{item}' at level '{level}'")
@@ -20,18 +21,19 @@ def validate_data(data, allowed_keys, level=""):
                 return False
     return True
 
+
 # Sample nested JSON data
 data = {
     "key1": [
-        {"nested_key1": {"nested_key3": "[1,2,3,4]"}},
-        {"ELSE": "ELSE_VALUE"},
+        {"nested_key1": {"nested_key3": [1, 2, 3, 4]}},
         {"nested_key2": {"nested_key4": "nested_value4"}},
+        {"ELSE": "ELSE_VALUE"},
     ],
     "key2": {
         "nested_key1": {"nested_key3": "nested_value3"},
-        "nested_key2": {"nested_key4": "nested_value4"}
+        "nested_key2": {"nested_key4": "nested_value4"},
     },
-    "key3": "value3"
+    "key3": "value3",
 }
 
 # Define the allowed keys
@@ -39,16 +41,17 @@ allowed_keys = {
     "key1": {
         "nested_key1": ["nested_key3"],
         "nested_key2": ["nested_key4"],
-        "ELSE": []
+        "ELSE": [],
     },
     "key2": {
         "nested_key1": ["nested_key3"],
-        "nested_key2": ["nested_key4"]
+        "nested_key2": ["nested_key4"],
     },
-    "key3": []
+    "key3": [],
 }
 
 # Validate the data
 is_valid = validate_data(data, allowed_keys)
 print(is_valid)
+
 
