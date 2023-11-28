fb7ec5307419001e5a48048b1af1ec8345164172
Slawomir Serafin <margolek555@gmail.com>
2023-11-28 15:54:27 +0100

    add functionality to check if element with ELSE key is the last item in the list

--- threshold-handler/validating_nested_json.py
+++ threshold-handler/validating_nested_json.py
@@ -1,21 +1,31 @@
-def validate_data(data, allowed_keys):
+
+
+def validate_data(data, allowed_keys, level=""):
     for key in data.keys():
         if key not in allowed_keys:
+            print(f"Error: Key '{key}' not allowed at level '{level}'")
             return False
         if isinstance(data[key], list):
-            for item in data[key]:
-                if not validate_data(item, allowed_keys[key]):
+            for index, item in enumerate(data[key]):
+                if not isinstance(item, dict):
+                    print(f"Error: Invalid item '{item}' at level '{level}'")
+                    return False
+                if "ELSE" in item and (index != len(data[key]) - 1):
+                    print(f"Error: 'ELSE' key is not the last item in the list at level '{level}.{key}'")
+                    return False
+                if not validate_data(item, allowed_keys[key], level=f"{level}.{key}"):
                     return False
         elif isinstance(data[key], dict):
-            if not validate_data(data[key], allowed_keys[key]):
+            if not validate_data(data[key], allowed_keys[key], level=f"{level}.{key}"):
                 return False
     return True
 
 # Sample nested JSON data
 data = {
     "key1": [
-        {"nested_key1": {"nested_key3": "nested_value3"}},
-        {"nested_key2": {"nested_key4": "nested_value4"}}
+        {"nested_key1": {"nested_key3": "[1,2,3,4]"}},
+        {"ELSE": "ELSE_VALUE"},
+        {"nested_key2": {"nested_key4": "nested_value4"}},
     ],
     "key2": {
         "nested_key1": {"nested_key3": "nested_value3"},
@@ -28,7 +38,8 @@ data = {
 allowed_keys = {
     "key1": {
         "nested_key1": ["nested_key3"],
-        "nested_key2": ["nested_key4"]
+        "nested_key2": ["nested_key4"],
+        "ELSE": []
     },
     "key2": {
         "nested_key1": ["nested_key3"],
